fa3a8fd8d696e638833af004946f441aa7075bc7
dany <dany19991@yahoo.com>
2019-08-31 16:13:35 +0300

    update widget from app . click on widget to go to app (does not close app on any action inside it).

--- Widgettest_1/app/src/main/java/com/example/widgettest_1/MainActivity.java
+++ Widgettest_1/app/src/main/java/com/example/widgettest_1/MainActivity.java
@@ -5,19 +5,22 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtview_item1;
+    TextView txtview_item2;
+    static String current_item = "item1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +47,44 @@ public class MainActivity extends AppCompatActivity {
                 sendBroadcast(intent);
             }
         });
+
+
+        txtview_item1 = (TextView)findViewById(R.id.text_item1);
+        txtview_item2 = (TextView)findViewById(R.id.text_item2);
+
+
+        txtview_item1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "item1",Toast.LENGTH_SHORT).show();
+                current_item = "item1";
+                UpdateAllWidgets();
+            }
+        });
+
+        txtview_item2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "item2",Toast.LENGTH_SHORT).show();
+                current_item = "item2";
+                UpdateAllWidgets();
+            }
+        });
+
+    }
+
+
+    private void UpdateAllWidgets ()
+    {
+        /* update all widgets */
+        Intent intent = new Intent(MainActivity.this, MyAppWidget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        // Use an array and EXTRA_APPWIDGET_IDS instead of AppWidgetManager.EXTRA_APPWIDGET_ID,
+        // since it seems the onUpdate() is only fired on that:
+        int[] ids = AppWidgetManager.getInstance(getApplication())
+                .getAppWidgetIds(new ComponentName(getApplication(), MyAppWidget.class));
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+        sendBroadcast(intent);
     }
 
     @Override
@@ -70,11 +111,12 @@ public class MainActivity extends AppCompatActivity {
 
     static public String getTimeFromApp ()
     {
-
-
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         //System.out.println( sdf.format(cal.getTime()) );
-        return sdf.format(cal.getTime());
+
+        //return sdf.format(cal.getTime());
+        return current_item;
     }
+
 }
--- Widgettest_1/app/src/main/java/com/example/widgettest_1/MyAppWidget.java
+++ Widgettest_1/app/src/main/java/com/example/widgettest_1/MyAppWidget.java
@@ -32,7 +32,7 @@ public class MyAppWidget extends AppWidgetProvider {
 
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
-        views.setOnClickPendingIntent(R.id.appwidget_text,pendingIntent);
+        views.setOnClickPendingIntent(R.id.widget_layout,pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
